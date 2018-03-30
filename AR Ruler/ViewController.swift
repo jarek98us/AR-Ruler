50fe096a50d0d43e882b6b667c80f8f33fd3f582
Jarek <jarek@jarek-mac.local>
2018-03-30 14:25:27 +0200

    AR Ruler implementation (POC) completed.

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -14,20 +14,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    var textNodes = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,34 +44,71 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            if let hitResult = hitResults.first {
+                addDot(at: hitResult)
+            }
+        }
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    func addDot(at hitResult: ARHitTestResult) {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        dotGeometry.materials = [material]
         let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
         
+        node.position = SCNVector3(
+            x: hitResult.worldTransform.columns.3.x,
+            y: hitResult.worldTransform.columns.3.y,
+            z: hitResult.worldTransform.columns.3.z)
+        
+        node.geometry = dotGeometry
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        dotNodes.append(node)
+
+        if dotNodes.count >= 2 {
+            calculateDistance(to: dotNodes.count - 1)
+        }
+        // sceneView.autoenablesDefaultLighting = true
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func calculateDistance(to nodeIdx: Int) {
+        let start = dotNodes[nodeIdx - 1].position
+        let end = dotNodes[nodeIdx].position
         
+        let distance = sqrt(pow(end.x - start.x, 2) + pow(end.y - start.y, 2) + pow(end.z - start.z, 2)) * 100
+        print("start: \(start)")
+        print("end: \(end)")
+        let distanceText = String(format: "%.2f", distance) + " cm"
+        displayText(text: distanceText, at: end)
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func displayText(text: String, at position: SCNVector3) {
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
         
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+        textNodes.append(textNode)
+    }
+    
+    @IBAction func cleanButtonPressed(_ sender: UIBarButtonItem) {
+        for node in dotNodes {
+            node.removeFromParentNode()
+        }
+        
+        for node in textNodes {
+            node.removeFromParentNode()
+        }
+        
+        dotNodes.removeAll()
+        textNodes.removeAll()
     }
 }
